2b3bc01781e5ceeccc5d2ce521acdd01323bdbdd
Alex Gavrilov <aleksei.gavrilov@openinnovation.ai>
2025-08-12 14:58:12 +0400

    dependencies

---
+++ backend/app/dependencies.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+from fastapi import Depends
+from app.repositories.interfaces.IDeploymentRepository import IDeploymentRepository
+from app.repositories.implementations.DeploymentRepository import DeploymentRepository
+from app.services.interfaces.IDeploymentsService import IDeploymentsService
+from app.services.implementations.DeploymentsService import DeploymentsService
+
+def get_deployment_repository() -> IDeploymentRepository:
+    return DeploymentRepository()
+
+def get_deployments_service(
+    repo: IDeploymentRepository = Depends(get_deployment_repository),
+) -> IDeploymentsService:
+    return DeploymentsService(repo)
+
+@lru_cache
+def get_deployment_repository_singleton() -> IDeploymentRepository:
+    return DeploymentRepository()
+
+@lru_cache
+def get_deployments_service_singleton() -> IDeploymentsService:
+    return DeploymentsService(get_deployment_repository_singleton())
