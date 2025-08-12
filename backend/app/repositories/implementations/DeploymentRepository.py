f44488ff42ea9e017c59924ce3fc43e5726c47f4
Alex Gavrilov <aleksei.gavrilov@openinnovation.ai>
2025-08-12 14:51:38 +0400

    in memory implementation

--- backend/app/repositories/implementations/DeploymentRepository.py
+++ backend/app/repositories/implementations/DeploymentRepository.py
@@ -3,11 +3,21 @@ from app.repositories.entities.DeploymentEntity import DeploymentEntity
 
 
 class DeploymentRepository(IDeploymentRepository):
+    def __init__(self):
+        self.d = {}
+
     def get_by_id(self, id: str) -> DeploymentEntity:
-        pass
+        if id not in self.d: return None
+
+        return self.d[id]
 
     def create(self, entity: DeploymentEntity) -> DeploymentEntity:
-        pass
+        self.d[entity.id] = entity
+        return entity
 
     def update(self, entity: DeploymentEntity) -> DeploymentEntity:
-        pass
+        self.d[entity.id] = entity
+        return entity
+
+    def delete(self, id: str) -> None:
+        self.d.pop(id, None)
