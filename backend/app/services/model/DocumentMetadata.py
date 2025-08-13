b72e60522a459516f05264e0c8da9a1026948a1b
Alex Gavrilov <alex@talentmesh.com>
2025-08-14 00:56:01 +0400

    fix type

--- backend/app/services/model/DocumentMetadata.py
+++ backend/app/services/model/DocumentMetadata.py
@@ -1,5 +1,8 @@
+from typing import List
+
+
 class DocumentMetadata:
     agreement_type: str
     jurisdiction: str
-    geography_mentioned: list[str]
+    geography_mentioned: List[str]
     industry: str
