97e05234fda64dbdc9e9060b8c1075c4e2170c14
Alex Gavrilov <alex@talentmesh.com>
2025-08-15 20:31:20 +0400

    error handling

--- frontend/src/pages/DashboardPage/components/GoverningLawChart.tsx
+++ frontend/src/pages/DashboardPage/components/GoverningLawChart.tsx
@@ -3,9 +3,16 @@ import Typography from "@shared/components/Typography";
 import Box from "@shared/components/Box";
 import PieChart from "@shared/components/charts/PieChart";
 import { useCountries } from "@entities/dashboardStats/queries/useCountries";
+import Loader from "@shared/components/Loader";
+import Alert from "@shared/components/Alert";
 
 export const GoverningLawChart: React.FC = () => {
-  const { data = [] } = useCountries();
+  const { isLoading, isError, data } = useCountries();
+
+  if (isLoading) return <Loader />;
+  if (isError)
+    return <Alert severity="error">Error loading governing law</Alert>;
+  if (!data) return <Alert severity="error">No data available</Alert>;
 
   const pieData = React.useMemo(
     () => data.map(({ category, count }) => ({ name: category, value: count })),
